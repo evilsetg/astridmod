6a56b5cce06ea48f576c1bb37f8777666e846275
Arvid Krein <arvidkrein@zedat.fu-berlin.de>
2025-07-30 11:36:21 +0200

    Add numerous improvements
    - Add baue alias to build nodes
    - Add dynamic prelude table to automatically include code in program node
    - Add node alias table for mineclonia node names
    - Add function shard item that holds code to be included in program node
      (Must be within first 9 inventory slots (hotbar) to be loaded)
    - Inhibit digging of program nodes
    - Change program node texture to computery look
    - Make formspecs a little nicer
    - Give sonic chatcommand boosted jump height

--- init.lua
+++ init.lua
@@ -1,66 +1,124 @@
-local function set_formspec(meta,code)
-   code = code or ""
-   is_error = meta:get_string("error") == "true"
-   meta:set_string("code", code)
-   meta:set_string("formspec",
-                   "formspec_version[4]" ..
-                   "size[25,25]" ..
-                   "label[1,1;Enter code]" ..
-                   "style_type[label;textcolor=red]" ..
-                   (is_error and "label[16,1;ERROR!]" or "") ..
-                   "style_type[label;textcolor=]" ..
-                   "button_exit[20,1;2,1;exit;exit]" ..
-                   "style[save;textcolor=green]" ..
-                   "button[22,22;2,2;save;save]" ..
-                   "textarea[1,2;20,20;x;here:;".. code .."]" ..
-                   "button[1,22;20,2;enter;Done!]")
+-- chatcommands
+core.register_chatcommand("sonic", {
+                             func = function(name, param)
+                                local player = core.get_player_by_name(name)
+                                player:set_physics_override({
+                                      speed = 50.0,
+                                      jump = 2.0
+                                })
+                             end,
+})
+
+core.register_chatcommand("unsonic", {
+                             func = function(name, param)
+                                local player = core.get_player_by_name(name)
+                                player:set_physics_override({
+                                      speed = 1.0,
+                                      jump = 1.0
+                                })
+                             end,
+})
+
+-- nodes
+
+-- formspec for program_node
+local function program_node_formspec(code,is_error)
+   local code = code or ""
+   local is_error = (is_error == "true")
+   local formspec = "formspec_version[4]" ..
+      "size[25,25]" ..
+      "label[1,1;Bitte Code eingeben!]" ..
+      "style_type[label;textcolor=red]" ..
+      (is_error and "label[16,1;FEHLER!]" or "") ..
+      "style_type[label;textcolor=]" ..
+      "button_exit[21.5,0.5;3,1;exit;Schließen]" ..
+      "style[save;textcolor=green]" ..
+      "button[21.5,22;3,2;save;Speichern]" ..
+      "textarea[1,2;20,20;code;hier:;".. code .."]" ..
+      "button[1,22;20,2;enter;Ausführen!]"
+   return formspec
 end
 
 
 core.register_node("astridmod:virus", {
                       description = "Eat the world",
                       tiles = {"astridmod_virus_node.png"},
-                      groups = {fleshy = 30, cracky=3},
-                      on_timer = function(pos)
-                         -- local x = vector.new(1,0,0)
-                         -- local y = vector.new(0,1,0)
-                         -- local z = vector.new(0,0,1)
-                         -- for _,p in pairs({x,-x,y,-y,z,-z}) do
-                         --    local inode = core.get_node(pos+p)
-                         --    if not (inode.name == "astridmod:virus" or inode.name == "air") then
-                         --       core.set_node(pos+p, { name = "astridmod:virus"})
-                         --    end
-                         -- end
-                         return false
-                      end,
-                      on_construct = function(pos, placer, itemstack, pointed_thing)
-                         timer = core.get_node_timer(pos)
-                         timer:start(4.0)
-                      end
+                      groups = {fleshy = 30, cracky=3}
 })
-                      
 
+-- include function_shards in player inventory into program_block prelude
+function fshard_prelude(player)
+   local inv = player:get_inventory()
+   local main_inv = inv:get_list("main")
+   local acc = ""
+   for i = 1,9 do -- check in hotbar slots
+      local stack = main_inv[i]
+      if stack:get_name() == "astridmod:function_shard" then
+         acc = acc .. stack:get_meta():get_string("text") .. "\n"
+      end
+   end
+   return acc
+end
+
+-- convenience functions for program_node
+local nodetable = {
+   luft = "air",
+   stein = "mcl_core:stone",
+   erde = "mcl_core:dirt",
+   kies = "mcl_core:gravel",
+   sand = "mcl_core:sand",
+   eisen = "mcl_core:ironblock",
+   gold = "mcl_core:goldblock",
+   diamant = "mcl_core:diamondblock",
+   glas = "mcl_core:glass",
+   weiß = "mcl_wool:white",
+   silber = "mcl_wool:silver",
+   grau = "mcl_wool:gray",
+   schwarz = "mcl_wool:black",
+   lila = "mcl_wool:purple",
+   blau = "mcl_wool:blue",
+   hellblau = "mcl_wool:light_blue",
+   cyan = "mcl_wool:cyan",
+   gruen = "mcl_wool:green",
+   gelbgruen = "mcl_wool:lime",
+   gelb = "mcl_wool:yellow",
+   braun = "mcl_wool:brown",
+   orange = "mcl_wool:orange",
+   rot = "mcl_wool:red",
+   magenta = "mcl_wool:magenta",
+   pink = "mcl_wool:pink"
+}
+
+local function baue(pos, name)
+   core.set_node(pos, {name = name})
+end
+
+local static_prelude = "local x = vector.new(1,0,0)\n" ..
+   "local y = vector.new(0,1,0)\n" ..
+   "local z = vector.new(0,0,1)\n"
+
+for k,v in pairs(nodetable) do
+   static_prelude = static_prelude ..
+      "local " .. k .. " = \"" .. v .. "\"\n"
+end
+
+-- program node
 core.register_node("astridmod:program_node", {
                       description = "Programmable block",
-                      tiles = {"astridmod_program_node.png"},
+                      tiles = {"astridmod_program_node_top.png",
+                               "astridmod_program_node_bottom.png",
+                               "astridmod_program_node_face.png"
+                      },
                       groups = {cracky = 3},
+                      diggable = false,
                       after_place_node = function(pos, placer, itemstack, pointed_thing)
                          if placer and placer:is_player() then
                             local meta = core.get_meta(pos)
                             meta:set_string("owner", placer:get_player_name())
-                            core.chat_send_player(placer:get_player_name(), "Hallo!")
                          end
                          local meta = core.get_meta(pos)
-                         meta:set_string("formspec",
-                                         "formspec_version[4]" ..
-                                         "size[25,25]" ..
-                                         "label[1,1;Enter code]" ..
-                                         "button_exit[20,1;2,1;exit;exit]" ..
-                                         "style[save;textcolor=green]" ..
-                                         "button[22,22;2,2;save;save]" ..
-                                         "textarea[1,2;20,20;x;here:;]" ..
-                                         "button[1,22;20,2;enter;Done!]")
                          meta:set_string("error", "false")
+                         meta:set_string("formspec", program_node_formspec("", "false"))
                       end,
                       on_receive_fields = function(pos, formname, fields, player)
                          if fields.quit then
@@ -68,47 +126,35 @@ core.register_node("astridmod:program_node", {
                          end
                          if fields.enter then
                             local meta = core.get_meta(pos)
-                            local prelude = "local x = vector.new(1,0,0)" ..
-                               "local y = vector.new(0,1,0)" ..
-                               "local z = vector.new(0,0,1)" ..
-                               "local pos = ..."
-                            local f = loadstring(prelude .. fields.x)
-                            status, res = pcall(f, pos)
+                            local ptable = { pos = pos, player = player, baue = baue }
+
+                            local prelude = static_prelude ..
+                               "local ptable = ...\n"
+                            for k,_ in pairs(ptable) do
+                               prelude = prelude ..
+                                  "local " .. k .. " = " .. "ptable." .. k .. "\n"
+                            end
+                            prelude = prelude .. "\n" .. fshard_prelude(player)
+
+                            local f = loadstring(prelude ..
+                                                 fields.code)
+                            status, res = pcall(f, ptable)
+
                             if status == false then
-                               print("ERROR in code!")
-                               print(res)
                                meta:set_string("error", "true")
                                core.chat_send_player(player:get_player_name(), res)
                             else
                                meta:set_string("error", "false")
                             end
-                            set_formspec(meta, fields.x)
+                            meta:set_string("code", fields.code)
+                            meta:set_string("formspec", program_node_formspec(fields.code, meta:get_string("error")))
                          end
                          if fields.save then
                             local meta = core.get_meta(pos)
-                            set_formspec(meta, fields.x)
+                            meta:set_string("code", fields.code)
+                            meta:set_string("formspec", program_node_formspec(fields.code, meta:get_string("error")))
                          end
                       end
-})
-
-core.register_chatcommand("sonic", {
-    func = function(name, param)
-        local player = core.get_player_by_name(name)
-        player:set_physics_override({
-            speed = 50.0, -- set gravity to 10% of its original value
-                           -- (0.1 * 9.81)
-        })
-    end,
-})
-
-core.register_chatcommand("unsonic", {
-    func = function(name, param)
-        local player = core.get_player_by_name(name)
-        player:set_physics_override({
-            speed = 1.0, -- set gravity to 10% of its original value
-                           -- (0.1 * 9.81)
-        })
-    end,
 })
 
 
@@ -127,7 +173,6 @@ core.register_node("astridmod:emperor_node", {
                          core.sound_play("astridmod_argenta")
                       end,
                       on_dig = function(pos, node, digger)
-                         print(node)
                          core.sound_play("astridmod_pain")
                          core.node_dig(pos, node, digger)
                       end
@@ -139,3 +184,36 @@ core.register_craft({
       output = "astridmod:program_node 3",
       recipe = { "mcl_core:dirt", "mcl_core:stone" }
 })
+
+local function use_function_shard(itemstack, user, pointed_thing)
+   local text = itemstack:get_meta():get_string("text")
+   local formspec = "formspec_version[4]" ..
+      "size[25,25]" ..
+      "label[1,1;Bitte Code eingeben!]" ..
+      "button_exit[21.5,0.5;3,1;exit;Schließen]" ..
+      "style[enter;textcolor=green]" ..
+      "textarea[1,2;20,20;code;hier:;" .. text .. "]" ..
+      "button[1,22;20,2;enter;Speichern!]"
+   core.show_formspec(user:get_player_name(), "function_shard_entry", formspec)
+end
+
+core.register_craftitem("astridmod:function_shard", {
+                           description = "Funktionsscherbe",
+                           inventory_image = "astridmod_function_shard.png",
+                           stack_max = 1,
+                           on_secondary_use = use_function_shard,
+                           on_place = use_function_shard
+})
+
+core.register_on_player_receive_fields(function(player, formname, fields)
+      if (formname == "function_shard_entry") then
+         local stack = player:get_wielded_item()
+         if (stack:get_name() == "astridmod:function_shard") then
+            local meta = stack:get_meta()
+            if fields.enter then
+               meta:set_string("text", fields.code)
+               player:set_wielded_item(stack)
+            end
+         end
+      end
+end)
